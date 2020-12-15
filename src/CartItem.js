e36a2ed96916404505d3e18b4b88c7a054987d68
anurag1001001 <anuragtripathi23may@gmail.com>
2020-12-15 15:09:56 +0530

    modify state

--- src/CartItem.js
+++ src/CartItem.js
@@ -2,6 +2,7 @@ import React from 'react';
 
 export default class CartItem extends React.Component{
     constructor(){
+        // Since we're inheriting Component from React and in javascript if we're inheriting something then we should call parent constructor first in app constructor 
         super();
         this.state = {
             price:999,
@@ -9,12 +10,42 @@ export default class CartItem extends React.Component{
             Qty:1,
             img: ''
         }
+        // Just google it give a read over bind()
         //  this.increaseQuantity = this.increaseQuantity.bind(this);
     }
 
     increaseQuantity = () => {
+        // state will get change by this way also but react will not reflect it to client side, so chnage the state use setState() 
+        // this.state.Qty += 1
         console.log('this', this.state);
+
+        
+        // setState method-1
+        // this.setState({
+        //     Qty: this.state.Qty + 1
+        // });
+
+
+        // setState method-2
+        //  Instead of passing object over there we'll pass callback function 
+        // Use this method when prevState is required
+        this.setState((prevState) =>{
+            // Calling setState() simply means that rerendering the page if state changes
+            return {
+                Qty: this.state.Qty + 1
+            }
+        });
     }
+
+    // decrease Quantity
+    decreaseQuantity = () => {
+        console.log('this', this.state);
+        // setState method-1
+        this.setState({
+            Qty: this.state.Qty - 1
+        });
+    }
+
     render(){
         // object destructuring
         const {price, title, Qty} = this.state
@@ -39,6 +70,7 @@ export default class CartItem extends React.Component{
                          alt="decrease"
                          className="action-icons" 
                          src="https://www.flaticon.com/premium-icon/icons/svg/2740/2740679.svg"
+                         onClick = {this.decreaseQuantity}
                           />
                         <img
                          alt="delete" 
