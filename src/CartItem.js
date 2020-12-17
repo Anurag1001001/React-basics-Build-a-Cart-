328b7d43602eec289e16339446577101b1ef3354
anurag1001001 <anuragtripathi23may@gmail.com>
2020-12-17 16:33:49 +0530

    Adding the Navbar component

--- src/CartItem.js
+++ src/CartItem.js
@@ -1,50 +1,49 @@
 import React from 'react';
 
-export default class CartItem extends React.Component{
-    increaseQuantity = () => {
-        // state will get change by this way also but react will not reflect it to client side, so chnage the state use setState() 
-        // this.state.Qty += 1
+const CartItem = (props) =>{
+    // increaseQuantity = () => {
+    //     // state will get change by this way also but react will not reflect it to client side, so chnage the state use setState() 
+    //     // this.state.Qty += 1
         
 
         
-        // setState method-1
-        this.setState({
-            Qty: this.state.Qty + 1
-        });
+    //     // setState method-1
+    //     this.setState({
+    //         Qty: this.state.Qty + 1
+    //     });
 
 
-        // setState method-2
-        //  Instead of passing object over there we'll pass callback function 
-        // Use this method when prevState is required
-        // this.setState((prevState) =>{
-        //     // Calling setState() simply means that rerendering the page if state changes
-        //     return {
-        //         Qty: this.state.Qty + 1
-        //     }
-        // });        
-    }
+    //     // setState method-2
+    //     //  Instead of passing object over there we'll pass callback function 
+    //     // Use this method when prevState is required
+    //     // this.setState((prevState) =>{
+    //     //     // Calling setState() simply means that rerendering the page if state changes
+    //     //     return {
+    //     //         Qty: this.state.Qty + 1
+    //     //     }
+    //     // });        
+    // }
 
-    // decrease Quantity
-    decreaseQuantity = () => {
-        // Object destructuring 
-        const {Qty} = this.state
-        if (Qty == 0){
-            return
-        }
-            console.log('this', this.state);
-            // setState method-1
-            this.setState({
-                Qty: this.state.Qty - 1
-            });       
-    }
+    // // decrease Quantity
+    // decreaseQuantity = () => {
+    //     // Object destructuring 
+    //     const {Qty} = this.state
+    //     if (Qty == 0){
+    //         return
+    //     }
+    //         console.log('this', this.state);
+    //         // setState method-1
+    //         this.setState({
+    //             Qty: this.state.Qty - 1
+    //         });       
+    // }
 
-    render(){
         // Every instance of cart Item will have a property called props and inside that props we'll get the data which we're passing attribute in cart.js(parent component)
-        console.log('this.props', this.props);
+        console.log('this.props', props);
         // object destructuring
         // product as  a object send hua h(as a props ) so ese likha h this.props.product 
-        const {price, title, Qty, id} = this.props.product;
-        const {onDeleteProduct} = this.props;
+        const {price, title, Qty, id} = props.product;
+        const {onDeleteProduct} = props;
         return(
             
             <div className ="cart-item">
@@ -61,13 +60,13 @@ export default class CartItem extends React.Component{
                          alt="increase"
                          className="action-icons" 
                          src="https://as2.ftcdn.net/jpg/01/07/62/07/500_F_107620769_UwNVSoXnKS4VNcOKoZjPohlEPn83oE38.jpg"
-                         onClick = {() => this.props.onIncreaseQuantity(this.props.product)}
+                         onClick = {() => props.onIncreaseQuantity(props.product)}
                          />
                         <img
                          alt="decrease"
                          className="action-icons" 
                          src="https://www.flaticon.com/premium-icon/icons/svg/2740/2740679.svg"
-                         onClick = {() => this.props.onDecreaseQuantity(this.props.product)}
+                         onClick = {() => props.onDecreaseQuantity(props.product)}
                           />
                         <img
                          alt="delete" 
@@ -79,7 +78,7 @@ export default class CartItem extends React.Component{
                 </div>
             </div>
         );
-    }
+    
 } 
 
 const styles = {
@@ -90,3 +89,5 @@ const styles = {
         background: '#777'
     }
 }
+
+export default CartItem;
